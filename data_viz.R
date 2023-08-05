ca349dc7960225f760888fd2ab217506c3b00599
godbleach <godbleach2425@gmail.com>
2023-08-05 02:54:56 +0000

    add some code

--- data_viz.R
+++ data_viz.R
@@ -2,5 +2,11 @@
 library(ggplot2)
 
 
+#first plot
 ggplot(diamonds, aes(carat, price)) +
   geom_point()
+
+#second plot
+ggplot(diamonds, aes(carat, prince))+
+  geom_point() +
+  geom_smooth()
--- hello_world.R
+++ hello_world.R
@@ -4,4 +4,9 @@ print("hello world")
 
 library(dplyr)
 mtcars %>%
-  select(1:5)
+  select(1:5) %>%
+  filter(mpg > 30)
+
+mtcars %>%
+  select(mpg) %>%
+  filter(mpg <30)
